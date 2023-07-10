2c3d165f7d7d5b7a4d652601a28d4b0cbc0abf6e
47aftab <133567134+47aftab@users.noreply.github.com>
2023-07-10 13:41:27 +0530

    Update akay13.cpp

--- akay13.cpp
+++ akay13.cpp
@@ -1,23 +1,61 @@
-
 // write a c++ code to check a graph is complete. represent the graph using adjacency list
 #include <iostream>
 #include <vector>
 using namespace std;
+// Number of vertices in the graph
+    int V = 5;
 
+void addEdge(vector<int> adj[], int u, int v) {
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
 
-bool isComplete(vector<vector<int>> adjList, int n){
-    
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < n; j++){
-            if (i != j && adjList[i][j] == 0){
-                return false;
-            }
+void printGraph(vector<int> adj[], int V) {
+    for (int v = 0; v < V; v++) {
+        cout << "Adjacency list of vertex " << v ;
+        
+        for (int edge : adj[v]) {
+            cout << " -> " << edge;
+        }
+        cout<<endl;
+    }
+}
+
+bool isGraphComplete(vector<int> adj[], int V) {
+    for (int v = 0; v < V; v++) {
+        if (adj[v].size() != V - 1) {
+            return false;
         }
     }
-    
     return true;
 }
 
+int main() {
+    
+
+    // Create an array of vectors to represent the graph
+    vector<int> adj[V];
+
+    // Adding edges to the graph
+    addEdge(adj, 0, 1);
+    addEdge(adj, 0, 4);
+    addEdge(adj, 1, 2);
+    addEdge(adj, 1, 3);
+    addEdge(adj, 1, 4);
+    addEdge(adj, 2, 3);
+    addEdge(adj, 3, 4);
+
+    // Print the adjacency list
+    printGraph(adj, V);
+    if(isGraphComplete(adj,V)){
+        cout<<"Graph is complete";
+    }
+    else{
+        cout<<"Graph is not complete";
+    }
+
+    return 0;
+}
 int main(){
     int n = 4;
     vector<vector<int>> adjList(n, vector<int>(n, 0));
